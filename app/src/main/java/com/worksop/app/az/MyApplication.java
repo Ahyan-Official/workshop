386761a34c3207dddb954494e42a344d83f09018
Azron Ahyan <azronahyan@gmail.com>
2022-11-15 23:57:53 +0500

    complete

--- app/src/main/java/com/worksop/app/az/MyApplication.java
+++ app/src/main/java/com/worksop/app/az/MyApplication.java
@@ -3,12 +3,14 @@ package com.worksop.app.az;
 import android.app.Application;
 
 import com.google.android.gms.ads.MobileAds;
+import com.onesignal.OneSignal;
 
 
 public class MyApplication extends Application {
 
 
     private static MyApplication mInstense;
+    private static final String ONESIGNAL_APP_ID = "70e573d0-3bc6-4ded-870d-68ed287a66c0";
 
 
     public MyApplication(){
@@ -27,7 +29,11 @@ public class MyApplication extends Application {
 
         MobileAds.initialize(this);
 
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
 
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
 //        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
 //
 //        // OneSignal Initialization
